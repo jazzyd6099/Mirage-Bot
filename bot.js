2bb3e435a37b3cdd228abf87da27cbb1ba10d16e
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-04-16 15:26:22 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = "!";
 
 client.on("ready", () => {
-  console.log("I am ready!");
+  console.log("You got bamboozled!");
+  
+  client.user.setActivity("Bamboozling you!"); 
+       client.user.setPresence({ activity: { name: 'Bamboozling you!' }, status: 'online' })
+  .then(console.log)
+  .catch(console.error);
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
+  
+   if (!message.content.startsWith(prefix)) return;
+  
+  if (message.content.startsWith(prefix + "ping")) {
     message.channel.send("pong!");
-  }
+    else
+      if(message.content.startsWith(prefix + "server")) {
+        message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
 });
 
  
