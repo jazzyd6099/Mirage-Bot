49cdc1f6ca45cf4bba88830e5af6f43bf6192a3d
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-04-16 15:39:28 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,23 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const prefix = "!";
 
 client.on("ready", () => {
-  console.log("I am ready!");
+  console.log("You got bamboozled!");
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  } else
-    if (message.content.startsWith("Hi")) {
+
+    if (message.content.startsWith("Hi Mirage")) {
         message.channel.send("Hello!");
+   }
+});
+client.on("message", (message) => {
+
+  if (!message.content.startsWith(prefix)) return;
+  
+    if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("pong!");
   }
 });
 
