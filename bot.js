e5c539a72f6d22ab0245cad36ea45f1045444ce3
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-04-16 15:44:04 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,11 +2,34 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = "!";
 
+//embedColors
+
+const embedRed = 0xff0000
+const embedOrange = 0xff790c
+const embedYellow = 0xffff00
+const embedGreen = 0x00ff00
+const embedBlue = 0x0064ff
+const embedPurple = 0x6a00b0
+const embedMagenta = 0x9600ff
+const embedPink = 0xff00ff
+const embedBlack = 0x000000
+const embedWhite = 0xffffff
+const embedGray = 0x777777
+
+var x = [
+    1,
+    2,
+    3,
+    4,
+    5,
+]
+
+
 client.on("ready", () => {
   console.log("You got bamboozled!");
   
-	client.user.setActivity("Bamboozling"); 
-       client.user.setPresence({ activity: { name: 'Bamboozling' }, status: 'online' })
+	client.user.setActivity("Porkchops"); 
+       client.user.setPresence({ activity: { name: 'Porkchops' }, status: 'online' })
   .then(console.log)
   .catch(console.error);
 });
@@ -23,6 +46,9 @@ client.on("message", (message) => {
   
     if (message.content.startsWith(prefix + "ping")) {
     message.channel.send("pong!");
+    } else
+	if (message.content.startsWith(prefix + "server")) {
+	message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
   }
 });
 
