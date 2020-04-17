ab4dce37aedfa86a09ec7acc72a628217278dce0
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-04-17 15:09:16 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -86,10 +86,6 @@ client.on("message", (message) => {
     if (message.content.startsWith(prefix + "ping")) {
     message.channel.send("pong!");
     } else
-	    
-	if (message.content.startsWith(prefix + "server")) {
-	message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-	} else
 		if (message.content.startsWith(prefix + "help")) {
 			message.author.send("Hey there! Need help with my commands? Here you go buddy!");
 		} else
