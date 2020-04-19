fe62c09011fab27112f6d834c52de05d0845ad0a
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-04-18 20:04:51 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = "!";
+const activities = require('./jsons/activity');
 
 //embedColors
 
@@ -87,9 +88,9 @@ var loots = [ 'A Med Kit!',
 client.on("ready", () => {
   console.log("You got bamboozled!");
   
-	client.user.setActivity("Eating Porkchops"); 
-       client.user.setPresence({ activity: { name: 'Eating Porkchops' }, status: 'online' })
-  .then(console.log)
+const activity = activities[Math.floor(Math.random() * activities.length)];
+client.user.setPresence({ activity: { name: activity.text, type: activity.type }, status: 'online' })
+	   .then(console.log)
   .catch(console.error);
 });
 client.on('guildMemberAdd', member => {
