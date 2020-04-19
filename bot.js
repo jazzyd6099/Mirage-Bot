5bd56089995f28e12876b490ebe212ffdcb51418
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-04-19 00:58:57 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = "!";
 const activities = require('./jsons/activity');
-const permissionNeeded = "MANAGE_NICKNAMES";
+
 //embedColors
 
 
