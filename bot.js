072c8d7c7c817956816730c1152c326d8b9e82f1
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-05-10 15:15:44 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -151,7 +151,7 @@ client.on("message", (message) => {
 								message.channel.send({embed})
 							} else
 							if (message.content.startsWith(prefix+"love")) {
-								let lovedUser = message.mentions.users.first();
+								let lovedUser = message.mentions.members.first();
 					 if(message.mentions.users.size < 1) return message.reply("You forgot to mention someone for me to love! 🙁");
 								message.reply("test " + lovedUser);
 						} else
