4b44a95818d7a670c184fb97df05144a982737af
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-08-21 09:11:13 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -219,11 +219,11 @@ client.on('message', async(message) => {
 						if(message.content.startsWith(prefix + "8ball")) {
 							let question = args.slice(1).join(' ')
     							if (!question) return message.channel.send("You need to provide a question for the mighty 8ball!");
-							
-							  var embed = new Discord.MessageEmbed()
-							  .setColor(0x6a00b0)
-							  .setTitle(`Looking into the ball...`)
-							  .setDescription(fortunes[Math.floor(Math.random() * fortunes.length)])
+							message.channel.send({embed: {
+            		        color: embedPurple,
+            		        title: "Looking into the ball...",
+                  		description: (fortunes[Math.floor(Math.random() * fortunes.length)]),
+								                   }});
 						} else
 							if(message.content.startsWith(prefix + "coinflip")) {
 								      var coinflip = ['Heads!','Tails!'];
