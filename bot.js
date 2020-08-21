3f3651878159cf019d0868fecf45e3626bff6a9d
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2020-08-21 09:21:05 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -218,7 +218,7 @@ client.on('message', async(message) => {
 					}else
 						if(message.content.startsWith(prefix + "8ball")) {
 							
-						let question = args.slice(1).join(' ')
+						let question = args.slice(0).join(' ')
 					if (!question) return message.channel.send("You need to provide a question for the 8ball!");
 							
 							var embed = new Discord.MessageEmbed()
